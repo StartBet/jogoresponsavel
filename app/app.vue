3869d81e6507188d87f3aac6d2ede141cb71d4ac
Lucas Murta <lucas111murta@gmail.com>
2026-08-20 18:52:18 -0300

    feat(app): add favicon on project

--- app/app.vue
+++ app/app.vue
@@ -2,7 +2,15 @@
 useHead({
   htmlAttrs: {
     'data-theme': 'dark'
-  }
+  },
+  title: 'Jogo Resposável | StartBet',
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: 'https://images.cometagaming.com/e0dc892c-f394-4e3c-69df-3e4290338400/banDesktop'
+    }
+  ]
 })
 </script>
 
